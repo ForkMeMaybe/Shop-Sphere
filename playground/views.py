0b257f874d88a3522b81b575f1cbaa2cf77397ae
fork_me_maybe <forkmemaybe@gmail.com>
2024-12-01 02:23:18 +0530

    implement some admin models

--- playground/views.py
+++ playground/views.py
@@ -7,5 +7,4 @@ from tags.models import TaggedItem
 # Create your views here.
 def say_hello(request):
     queryset = TaggedItem.objects.get_tags_for(Product, 1)
-
     return render(request, "hello.html", {"name": "Piyush", "tags": list(queryset)})
