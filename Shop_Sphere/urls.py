59c96af67a18bfc0ed944f6982fd3d2f82c4ad02
fork_me_maybe <forkmemaybe@gmail.com>
2024-12-12 00:33:05 +0530

    Use class based views instead of function based views

--- Shop_Sphere/urls.py
+++ Shop_Sphere/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
@@ -22,6 +23,7 @@ admin.site.site_header = "Shop_Sphere Admin"
 admin.site.index_title = "Admin"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path("admin/", admin.site.urls),
+    path("playground/", include("playground.urls")),
+    path("store/", include("store.urls")),
 ] + debug_toolbar_urls()
--- store/views.py
+++ store/views.py
@@ -1,3 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.db.models import Count
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Collection, Product
+from .serializers import CollectionSerializer, ProductSerializer
 
-# Create your views here.
+
+class ProductList(APIView):
+    def get(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        if product.orderitems.count() > 0:
+            return Response(
+                {
+                    "error": "product cannot be deleted because it is associated with an order item"
+                },
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CollectionList(APIView):
+    def get(self, request):
+        queryset = Collection.objects.annotate(products_count=Count("products")).all()
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CollectionDetail(APIView):
+    def get(self, request, id):
+        collection = get_object_or_404(
+            Collection.objects.annotate(products_count=Count("products")), id=id
+        )
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        collection = get_object_or_404(
+            Collection.objects.annotate(products_count=Count("products")), id=id
+        )
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, id):
+        collection = get_object_or_404(
+            Collection.objects.annotate(products_count=Count("products")), id=id
+        )
+        if collection.products.count() > 0:
+            return Response(
+                {
+                    "error:": "Collection cannot be deleted because it includes one or more products"
+                },
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
