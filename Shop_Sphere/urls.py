11301a61bd18f87092ffdb32d32238aa45512cd1
fork_me_maybe <forkmemaybe@gmail.com>
2025-02-22 18:46:20 +0530

    chagnes

--- Shop_Sphere/urls.py
+++ Shop_Sphere/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
 ] + debug_toolbar_urls()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
